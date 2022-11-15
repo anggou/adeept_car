14c5c2708da19d80f8fd5df475e956df82e2826b
anggou <oldbay@kmou.ac.kr>
2022-11-15 14:44:32 +0900

    "before capture mark4"

--- server/test_write.py
+++ server/test_write.py
@@ -3,13 +3,16 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 def capture_spare():
+    while cap.isOpened():  # 초기화
+        keyValue = cv2.waitKey(0)
         filepath = "/home/pi/adeept_car/photos/train"
         _, image = cap.read()
         image = cv2.flip(image, -1)
         height, _, _ = image.shape
         save_image = image[int(height / 2):, :, :]
         cv2.imwrite("%s_%05d.png" % (filepath, 1), save_image)
-
+        if keyValue == ord('q'):
+            break
 if __name__ == '__main__':
     capture_spare()
     cv2.destroyAllWindows()
