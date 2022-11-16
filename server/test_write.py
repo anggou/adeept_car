818f2fe88692fbc1c7bc11f7ea65537cd0d3b619
anggou <oldbay@kmou.ac.kr>
2022-11-17 00:01:27 +0900

    "After capture mark5"

--- server/test_write.py
+++ server/test_write.py
@@ -1,41 +1,67 @@
 import cv2
-
-cap = cv2.VideoCapture(0)
-
-
 def capture_spare():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+    filepath = "/home/pi/adeept_car/photos/train"
     i = 0
-    while cap.isOpened():  # 초기화
-        keyValue = cv2.waitKey(0)
-        filepath = "/home/pi/adeept_car/photos/train"
+    carmera = "0"
+
+    while (cap.isOpened()):
+        keyValue = cv2.waitKey(10)
+
+        if keyValue == ord('q'):
+            break
+        elif keyValue == 1:
+            print("1")
+            carmera = "1"
+        elif keyValue == 2:
+            print("2")
+            carmera = "2"
+        elif keyValue == 3:
+            print("3")
+            carmera = "3"
+        elif keyValue == 4:
+            print("4")
+            carmera = "4"
+        elif keyValue == 5:
+            print("5")
+            carmera = "5"
+        elif keyValue == 6:
+            print("6")
+            carmera = "6"
+        elif keyValue == 0:
+            print("0")
+            carmera = "0"
+
         _, image = cap.read()
         image = cv2.flip(image, -1)
         height, _, _ = image.shape
         save_image = image[int(height / 2):, :, :]
+        save_image = cv2.resize(save_image, (200,66))
         cv2.imshow('Save', save_image)
-        if keyValue == ord('1'):
+
+        if carmera == "1":
             cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 1), save_image)
             i += 1
-        if keyValue == ord('2'):
+        elif carmera == "2":
             cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 2), save_image)
             i += 1
-        if keyValue == ord('3'):
+        elif carmera == "3":
             cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 3), save_image)
             i += 1
-        if keyValue == ord('4'):
+        elif carmera == "4":
             cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 4), save_image)
             i += 1
-        if keyValue == ord('5'):
+        elif carmera == "5":
             cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 5), save_image)
             i += 1
-        if keyValue == ord('6'):
+        elif carmera == "6":
             cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 6), save_image)
             i += 1
-        if keyValue == ord('0'):
+        elif carmera == "0":
             cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 0), save_image)
             i += 1
-        if keyValue == ord('q'):
-            break
 
 
 if __name__ == '__main__':
