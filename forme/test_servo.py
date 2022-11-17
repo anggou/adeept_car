b0bdf0a28f22deb2e44591c8a06a8783a3248fdb
anggou <oldbay@kmou.ac.kr>
2022-11-18 07:03:26 +0900

    "After capture mark5"

---
+++ forme/test_servo.py
@@ -0,0 +1,19 @@
+from __future__ import division
+import time
+# import Adafruit_PCA9685
+# import ultra
+import servo
+# import move
+
+
+
+
+if __name__ == '__main__':
+        pwm0_direction = 1
+        # move.motorStop()
+        servo.lookleft(150)
+        time.sleep(3)
+        # spare_capture()
+        # time.sleep(1)
+        servo.servo_init()
+        # carState = "go"
