17eda2ca545133168e6886b6c71206bb7000f122
anggou <oldbay@kmou.ac.kr>
2022-11-26 18:01:01 +0900

    "test"

--- forme/1111.py
+++ forme/1111.py
@@ -28,55 +28,42 @@ def spare_capture():
     model = load_model('/home/pi/adeept_car/forme/keras_model.h5')
     size = (224, 224)
     classes = ['Empty', 'Spindle_1', 'Spindle_2', 'Spindle_3', 'Spring_1', 'Spring_2', 'Spring_3']
-
-    while cap.isOpened():
-        ret, img = cap.read()
-        if not ret:
-            break
-
-        h, w, _ = img.shape
-        cx = h / 2
-        img = img[:, 200:200 + img.shape[0]]
-        img = cv2.flip(img, 1)
-        cv2.imshow('result', img)
-
-        img_input = cv2.resize(img, size)
-        img_input = cv2.cvtColor(img_input, cv2.COLOR_BGR2RGB)
-        img_input = (img_input.astype(np.float32) / 127.0) - 1
-        img_input = np.expand_dims(img_input, axis=0)
-
-        prediction = model.predict(img_input)
-        idx = np.argmax(prediction)
-        print("idx :", idx)
-        spare = classes[idx]
-
-        print("spare is:", spare)
-        if spare == 'Empty':
-            print("Empty")  # GUI로 보내기
-            result.append('Empty')
-        elif spare == 'Spindle_1':
-            print("Spindle_1")  # GUI로 보내기
-            result.append('Spindle_1')
-        elif spare == 'Spindle_2':
-            print("Spindle_2")  # GUI로 보내기
-            result.append('Spindle_2')
-        elif spare == 'Spindle_3':
-            print("Spindle_3")  # GUI로 보내기
-            result.append('Spindle_3')
-        elif spare == 'Spring_1':
-            print("Spring_1")  # GUI로 보내기
-            result.append('Spring_1')
-        elif spare == 'Spring_2':
-            print("Spring_2")  # GUI로 보내기
-            result.append('Spring_2')
-        elif spare == 'Spring_3':
-            print("Spring_3")  # GUI로 보내기
-            result.append('Spring_3')
-
-        # cv2.putText(img, text=classes[idx], org=(10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8, color=(255, 255, 255), thickness=2)
-
-        if cv2.waitKey(1) == ord('q'):
-            break
+    ret, img = cap.read()
+    h, w, _ = img.shape
+    cx = h / 2
+    img = img[:, 200:200 + img.shape[0]]
+    img = cv2.flip(img, 1)
+    cv2.imshow('result', img)
+    img_input = cv2.resize(img, size)
+    img_input = cv2.cvtColor(img_input, cv2.COLOR_BGR2RGB)
+    img_input = (img_input.astype(np.float32) / 127.0) - 1
+    img_input = np.expand_dims(img_input, axis=0)
+    prediction = model.predict(img_input)
+    idx = np.argmax(prediction)
+    spare = classes[idx]
+    print("spare is:", spare)
+    if spare == 'Empty':
+        print("Empty")  # GUI로 보내기
+        result.append('Empty')
+    elif spare == 'Spindle_1':
+        print("Spindle_1")  # GUI로 보내기
+        result.append('Spindle_1')
+    elif spare == 'Spindle_2':
+        print("Spindle_2")  # GUI로 보내기
+        result.append('Spindle_2')
+    elif spare == 'Spindle_3':
+        print("Spindle_3")  # GUI로 보내기
+        result.append('Spindle_3')
+    elif spare == 'Spring_1':
+        print("Spring_1")  # GUI로 보내기
+        result.append('Spring_1')
+    elif spare == 'Spring_2':
+        print("Spring_2")  # GUI로 보내기
+        result.append('Spring_2')
+    elif spare == 'Spring_3':
+        print("Spring_3")  # GUI로 보내기
+        result.append('Spring_3')
+    # cv2.putText(img, text=classes[idx], org=(10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8, color=(255, 255, 255), thickness=2)
 
 
 def setup():
